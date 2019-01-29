a86e479634496e137617b25ecd7802db948bc72a
Levi Terry <ldterry14@gmail.com>
2019-01-29 14:54:11 -0700

    Added half_arrow.c

---
+++ half_arrow.c
@@ -0,0 +1,38 @@
+// Author: LDT
+// Date: 29JAN2019
+// Title: half_arrow.c
+// Purpose: This program creates a half arrow of
+// the user's specified size.
+
+// Import Statements
+#include <stdio.h>
+
+// Main Code
+int main(void) {
+    int arrowBaseHeight;
+    int arrowBaseWidth;
+    int arrowHeadWidth;
+
+    printf("Enter arrow base height:\n");
+    scanf("%d", &arrowBaseHeight);
+
+    printf("Enter arrow base width:\n");
+    scanf("%d", &arrowBaseWidth);
+
+    printf("Enter arrow head width:\n");
+    scanf("%d", &arrowHeadWidth);
+    printf("\n");
+
+    // Draw arrow base (height = 3, width = 2)
+    printf( "**\n");
+    printf( "**\n");
+    printf( "**\n");
+
+    // Draw arrow head (width = 4)
+    printf( "****\n");
+    printf( "***\n");
+    printf( "**\n");
+    printf( "*\n");
+
+    return 0;
+}
