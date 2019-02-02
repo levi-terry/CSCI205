36a7d00613a91db4a33798b4ae835eed0883b88b
Levi Terry <ldterry14@gmail.com>
2019-02-02 14:26:29 -0700

    Added randomwalk.c

---
+++ randomwalk.c
@@ -0,0 +1,39 @@
+// Author: LDT
+// Date: 02FEB2019
+// Title: randomwalk.c
+// Description: This program uses an array
+// to visualize the randomness of a coin flip.
+
+// Import Statements
+#include <stdio.h>
+
+// Main Code Block
+int main(void){
+
+    // Global Variables
+    int coinFlips[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int flipCount;
+    int marker = 5; // To mark middle index of array
+    int flip;
+    //char goAgain = 'y';
+
+    printf("Enter the number of times you would like to flip the coin:\n");
+    scanf(" %d", &flipCount);
+
+    for(int i = 0; i < flipCount; i++){
+        flip = rand() % 2;
+        if(flip == 0){
+            marker += 1;
+            coinFlips[marker] += 1;
+        }
+        else if(flip == 1){
+            marker -= 1;
+            coinFlips[marker] += 1;
+        }
+    }
+
+    for(int i = 0; i < 11; i++){
+        printf("%d is : %d\n", i, coinFlips[i]);
+    }
+
+}
